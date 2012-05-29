29ca6849f2c7080f556c1d82b507ee3d804b47fd
Marc-Daniel Ortega <globulon@gmail.com>
2012-05-29 17:38:01 +0200

    [GAP-89 : GAP 120] First version of compiler plugin

---
+++ project/Build.scala
@@ -0,0 +1,44 @@
+import sbt._
+import Keys._
+
+object ApplicationDependencies {
+  lazy val ScalaVer = "2.9.1"
+}
+
+object BuildSettings {
+  import ApplicationDependencies._
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization        := "com.eligotech",
+    version             := "0.1-SNAPSHOT",
+    scalaVersion        := ScalaVer,
+    scalacOptions       := Seq("-unchecked", "-deprecation")
+  )
+}
+
+object Resolvers {
+  lazy val typesafeReleases = "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+  //lazy val scalaToolsRepo = "sonatype-oss-public" at "https://oss.sonatype.org/content/groups/public/"
+}
+
+object ApplicationBuild extends Build {
+  import ApplicationDependencies._
+  import Resolvers._
+  import BuildSettings._
+
+  val appDependencies = Seq(
+    "org.scala-lang"  % "scala-compiler"  % ScalaVer,
+    "org.scala-tools.testing" %% "specs"           % "1.6.9"  % "test" withSources(),
+    "junit"                    % "junit"           % "4.8.2"  % "test" withSources(),
+    "com.novocode"             % "junit-interface" % "0.7"    % "test"
+  )
+
+  
+ lazy val algorithms = Project(
+    "linter",
+    file("."),
+    settings =  buildSettings ++ 
+                Seq(resolvers += typesafeReleases) ++  
+                Seq(libraryDependencies ++= appDependencies) ++
+                Seq(scalacOptions in console in Compile <+= (packageBin in Compile) map { pluginJar =>"-Xplugin:"+pluginJar }) 
+  )
+}
---
+++ src/main/scala/com/eligotech/lint/LinterPlugin.scala
@@ -0,0 +1,78 @@
+package com.eligotech.lint
+
+import scala.reflect.generic.Flags
+import scala.tools.nsc.{Global, Phase}
+import scala.tools.nsc.plugins.{Plugin, PluginComponent}
+
+class LinterPlugin(val global: Global) extends Plugin {
+  import global._
+
+  val name = "linter"
+  val description = ""
+  val components = List[PluginComponent](LinterComponent)
+
+  private object LinterComponent extends PluginComponent {
+    import global._
+
+    val global = LinterPlugin.this.global
+
+    override val runsAfter = List("typer")
+
+    val phaseName = "linter"
+
+    override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
+      override def apply(unit: global.CompilationUnit): Unit = {
+        new LinterTraverser(unit).traverse(unit.body)
+      }
+    }
+
+    class LinterTraverser(unit: CompilationUnit) extends Traverser {
+      import definitions.{AnyClass, ObjectClass, Object_==, OptionClass, SeqClass}
+
+      val JavaConversionsModule: Symbol = definitions.getModule("scala.collection.JavaConversions")
+      val SeqLikeClass: Symbol = definitions.getClass("scala.collection.SeqLike")
+      val SeqLikeContains: Symbol = SeqLikeClass.info.member(newTermName("contains"))
+      val OptionGet: Symbol = OptionClass.info.member(nme.get)
+
+      def SeqMemberType(seenFrom: Type): Type = {
+        SeqLikeClass.tpe.typeArgs.head.asSeenFrom(seenFrom, SeqLikeClass)
+      }
+
+      def isSubtype(x: Tree, y: Tree): Boolean = {
+        x.tpe.widen <:< y.tpe.widen
+      }
+
+      def methodImplements(method: Symbol, target: Symbol): Boolean = {
+        method == target || method.allOverriddenSymbols.contains(target)
+      }
+
+      def isGlobalImport(selector: ImportSelector): Boolean = {
+        selector.name == nme.WILDCARD && selector.renamePos == -1
+      }
+
+      override def traverse(tree: Tree): Unit = tree match {
+        case Apply(eqeq @ Select(lhs, nme.EQ), List(rhs))
+          if methodImplements(eqeq.symbol, Object_==) && !(isSubtype(lhs, rhs) || isSubtype(rhs, lhs)) =>
+          val warnMsg = "Comparing with == on instances of different types (%s, %s) will probably return false."
+          unit.warning(eqeq.pos, warnMsg.format(lhs.tpe.widen, rhs.tpe.widen))
+
+        case Import(pkg, selectors)
+          if pkg.symbol == JavaConversionsModule && selectors.exists(isGlobalImport) =>
+          unit.warning(pkg.pos, "Conversions in scala.collection.JavaConversions._ are dangerous.")
+
+        case Apply(contains @ Select(seq, _), List(target))
+          if methodImplements(contains.symbol, SeqLikeContains) && !(target.tpe <:< SeqMemberType(seq.tpe)) =>
+          val warnMsg = "SeqLike[%s].contains(%s) will probably return false."
+          unit.warning(contains.pos, warnMsg.format(SeqMemberType(seq.tpe), target.tpe.widen))
+
+        case get @ Select(_, nme.get) if methodImplements(get.symbol, OptionGet) =>
+          if (!get.pos.source.path.contains("src/test")) {
+            unit.warning(get.pos, "Calling .get on Option will throw an exception if the Option is None.")
+          }
+
+        case _ =>
+          super.traverse(tree)
+      }
+    }
+  }
+}
